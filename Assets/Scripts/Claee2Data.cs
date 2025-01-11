c3c9b84141736c99a1a0a5905c3327ef70b24875
motoshin0414 <sourcegod910414@gmail.com>
2025-01-11 21:09:53 +0800

    Updata 認識資料類型

---
+++ Assets/Scripts/Claee2Data.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+/// <summary>
+/// 認識資料類型
+/// </summary>
+public class Claee2Data:MonoBehaviour
+{
+    /*
+    整數:儲存沒有小數點的數值 int
+    浮點數:儲存有小數點的數值 float 
+    字串:儲存文字，必須使用雙引號"" string EX:"源神"
+    布林值:儲存正與反的數值 bool 只有true 與false
+     */
+    public int coin = 1000;
+    public float movespeed = 3.5f;
+    public float trunspeed = 30.0f;
+    public string name = "源神";
+    public bool gameover = false;
+}
--- Assets/Scripts/Class1VarLearn.cs
+++ Assets/Scripts/Class1VarLearn.cs
@@ -4,11 +4,11 @@
 using UnityEngine;
 
 //修飾詞 類別關鍵字(腳本) 類別名稱(腳本名稱)
-public class Class1VarLearn
+public class Class1VarLearn:MonoBehaviour
 {
     //變數:定義一個記憶體內的空間來存放資料
-    //變數語法:變數資料類型 變數名稱;
+    //變數語法:變數資料類型 變數名稱 指定 預設值;(指定和預設值可以省略)
     //變數名稱:習慣用小寫開頭命名，駝峰式命名，規則與腳本名稱相同
-    int coin;
-
+    public int coin = 5000;
+    private int Level = 1;
 }
