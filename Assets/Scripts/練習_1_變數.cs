7cc302c24616b2a77bff0a36c2406c0437cc6fe9
motoshin0414 <sourcegod910414@gmail.com>
2025-01-15 18:02:08 +0800

    Update 完成作業練習
    2025/1/15完成

---
+++ Assets/Scripts/練習_1_變數.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class 練習_1_變數 : MonoBehaviour
+{
+    public ushort price = 999;
+    private float jumphigh = 77.5F;
+    public string arms = "三項之力";
+    private bool isgetdragonsoul = true;
+}
---
+++ Assets/Scripts/練習_2_資料類型.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class 練習_2_資料類型 : MonoBehaviour
+{
+    public float FloatData = 7.7f;
+    public double DoubleData = 0.12345f;
+    public decimal DecimalData = 1.23456789m;
+}
