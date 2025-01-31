238a10fc84119fdf56a8b3eaec984cb880ed51d0
motoshin0414 <sourcegod910414@gmail.com>
2025-01-31 17:33:22 +0800

    Update class 5

---
+++ Assets/Scripts/Class5Selection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+//命名空間:
+//將類別分類，類似倉庫的概念，不同倉庫可以用相同的名稱
+//語法:namespace 命名空間名稱{}
+namespace Motoshin
+{
+    /// <summary>
+    /// 課程5:Selection 判斷式
+    /// 1.if 判斷式
+    /// 2.switch 判斷式
+    /// </summary>
+
+    public class Class5Selection : MonoBehaviour
+    {
+        
+    }
+
+}
