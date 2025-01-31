dd6fcc5d930c7afc4bec748fc24a397c94bdec0f
motoshin0414 <sourcegod910414@gmail.com>
2025-02-01 01:00:34 +0800

    Update enum

---
+++ Assets/Scripts/Class5_2Enum.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+namespace Motoshin
+{
+    /// <summary>
+    /// 列舉:enumeration，C#內關鍵字為enum
+    /// </summary>
+    public class Class5_2Enum : MonoBehaviour
+    {
+        //列舉:定義下拉式選單的選項(通常為單選)
+        //語法:
+        //修飾詞 關鍵字 enum 列舉名稱 {列舉選項}
+        //定義一個列舉，為季節
+        private enum Season
+        {
+            Spring,Summer,Fall,Winter
+        }
+        [SerializeField , Header("季節")]
+        private Season season = Season.Summer;
+
+        private void Awake()
+        {
+            //取得列舉的值(Get)抓出值
+            Debug.Log(season);
+            //取得列舉的整數值
+            Debug.Log((int)season);
+
+            //設定列舉的值(Set)修改值
+        }
+
+    }
+}
+
