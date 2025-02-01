266510012cf22bf8f11aeb59496e0d5e73ba023e
motoshin0414 <sourcegod910414@gmail.com>
2025-02-01 15:36:13 +0800

    Update Class6While_for_foreach

--- Assets/Scripts/Class5_2Enum.cs
+++ Assets/Scripts/Class5_2Enum.cs
@@ -10,6 +10,7 @@ namespace Motoshin
         //語法:
         //修飾詞 關鍵字 enum 列舉名稱 {列舉選項}
         //定義一個列舉，為季節
+        //舉列都是數值，預設從0開始
         private enum Season
         {
             Spring,Summer,Fall,Winter
@@ -25,6 +26,11 @@ namespace Motoshin
             Debug.Log((int)season);
 
             //設定列舉的值(Set)修改值
+            season = Season.Winter;
+            Debug.Log(season);
+            //透過數值設定列舉
+            season = (Season)2;
+            Debug.Log(season);
         }
 
     }
---
+++ Assets/Scripts/Class6While_for_foreach.cs
@@ -0,0 +1,29 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+namespace motoshin
+{
+    /// <summary>
+    /// 反覆運算述式:迴圈
+    /// for、foreach、do、while
+    /// </summary>
+    public class Class6While_for_foreach : MonoBehaviour
+    {
+        private void Awake()
+        {
+            //迴圈請在一次性事件內使用:Awake、Start
+            //while迴圈:當布林值為true時會持續執行
+            //while(布林值) { 程式區段 }
+            //無限迴圈:布林值一直為true
+            //宣告區域變數 i 為 0
+            int i = 0;
+            while(i < 5)
+            {
+                Debug.Log($"<color = #F33>while 迴圈，i :{i}</color>");
+                i++;
+            }
+        }
+        
+    }
+}
+
+
