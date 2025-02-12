d51529ce43dd0d6ca1952a93c0c25cb0bc80fec0
motoshin0414 <sourcegod910414@gmail.com>
2025-02-12 16:36:36 +0800

    update Property

---
+++ Assets/Scripts/Class6_2_Array.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+/// <summary>
+/// 陣列 Array
+/// </summary>
+
+public class Class6_2_Array : MonoBehaviour
+{
+
+    private int[][] numbers = new int[2][];
+
+    private int[][,] counts = new int[2][,];
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        //不規則陣列基本寫法
+        numbers[0] = new int[] { 1,2,6};
+        numbers[1] = new int[] { 9, 0 };
+
+        //取得數字6
+        Debug.Log($"<color=#3ff>不規則陣列的數字6:{numbers[0][2]}</color>");
+    }
+
+    
+}
---
+++ Assets/Scripts/Class7_1_Property.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Class7_1_Property : MonoBehaviour
+{
+    //公開的變數:可以讓外部存取與寫入
+    public float MoveSpeed = 3.5f;
+
+    //私人的變數:不允許外部讀取與寫入
+    private float TurnSpeed = 12.5f;
+}
