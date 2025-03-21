b917b52f98e510b657c9610f083dd58e5aa60b82
motoshin0414 <sourcegod910414@gmail.com>
2025-03-21 22:28:50 +0800

    Update Class15介面_認識介面

---
+++ Assets/Scripts/Class15_Interface.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using motoshin.Tools;
+
+
+namespace motoshin.Class15
+{
+    public class Class15_Interface : MonoBehaviour
+    {
+        public object invertoryFirst;
+
+        private void Awake()
+        {
+            int random = Random.Range(0, 3);
+            LogSystem.LogWithColor($"隨機數字:{random}", "#FFF");
+
+            if (random == 1) invertoryFirst = new Prop();
+            else if (random == 2) invertoryFirst = new Equipment();
+            else if (random == 3) invertoryFirst = new Map();
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                //如果第一格是道具，就使用道具
+                if (invertoryFirst is Prop) ((Prop)invertoryFirst).Use();
+                //如果第一格是裝備，就使用裝備
+                if (invertoryFirst is Equipment) ((Equipment)invertoryFirst).Use();
+                //如果第一格是地圖，就使用地圖
+                if (invertoryFirst is Map) ((Map)invertoryFirst).Use();
+
+            }
+        }
+    }
+
+    public class Prop
+    {
+        public void Use() 
+        {
+            LogSystem.LogWithColor("使用道具，恢復體力", "#F01");
+            
+        }
+
+    }
+    public class Equipment
+    {
+        public void Use()
+        {
+            LogSystem.LogWithColor("使用裝備，到對應位置", "#F01");
+            
+        }
+    }
+
+    public class Map
+    {
+        public void Use()
+        {
+            LogSystem.LogWithColor("使用地圖，開啟地圖功能", "#F01");
+        }
+    }
+}
+
---
+++ Assets/Scripts/Class15_InterfaceObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using motoshin.Tools;
+
+namespace motoshin.Class15
+{
+    /// <summary>
+    /// 介面
+    /// </summary>
+    public class Class15_InterfaceObject : MonoBehaviour
+    {
+        //介面的使用步驟:
+        //1.定義介面與介面成員
+        //2.類別實作介面
+        //3.實作介面成員
+
+        //步驟1.
+        //帶有「使用」功能的介面，遊戲內可被使用的物品
+        //使用I開頭命名
+        public interface IUse
+        {
+            //不用宣告方法主體，不用寫大括號
+            public void Use();
+        }
+
+        //步驟2.
+        //C# 單一繼承，多重實作介面
+        public class Weapon : IUse
+        {
+            public void Use()
+            {
+                LogSystem.LogWithColor("使用武器，釋放武器技能", "#F73");
+
+            }
+        }
+        public class Potion : IUse
+        {
+            public void Use()
+            {
+                LogSystem.LogWithColor("使用藥水，恢復魔力", "#F73");
+
+            }
+        }
+        public class Chest : IUse
+        {
+            public void Use()
+            {
+                LogSystem.LogWithColor("使用寶箱，獲得隨機道具", "#F73");
+
+            }
+        }
+    }
+
+}
+
