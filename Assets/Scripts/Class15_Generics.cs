002d4c45d43d50e4a552590d0e111bc3429b468e
motoshin0414 <sourcegod910414@gmail.com>
2025-03-22 13:09:05 +0800

    Update Class15_Generics

--- Assets/Scripts/Class15_Generics.cs
+++ Assets/Scripts/Class15_Generics.cs
@@ -10,11 +10,12 @@ namespace motoshin.Class15
     {
         private void Awake()
         {
+            #region 不使用泛型
             int NumA = 7, NumB = 11;
             LogSystem.LogWithColor($"數字A與B:{NumA} | {NumB}", "#FFF");
             SwapNumber(ref NumA, ref NumB);
             LogSystem.LogWithColor($"數字A與B:{NumA} | {NumB}", "#FFF");
-            LogSystem.LogWithColor($"數字A與B:{NumA} | {NumB}", "#FFF");
+
 
             char charA = '嗨', charB = '嘿';
             LogSystem.LogWithColor($"字元A與B:{charA} | {charB}", "#FFF");
@@ -25,8 +26,28 @@ namespace motoshin.Class15
             LogSystem.LogWithColor($"物件A與B:{objA} | {objB}", "#FFF");
             SwapObj(ref objA, ref objB);
             LogSystem.LogWithColor($"物件A與B:{objA} | {objB}", "#FFF");
+            #endregion
+
+            bool boolA = true, boolB = false;
+            LogSystem.LogWithColor($"A與B:{boolA} | {boolB}", "#FFF");
+            Swap<bool>(ref boolA, ref boolB);
+            LogSystem.LogWithColor($"A與B:{boolA} | {boolB}", "#FFF");
+
+            byte byteA = 1, byteB = 9;
+            LogSystem.LogWithColor($"A與B:{byteA} | {byteB}", "#FFF");
+            Swap<byte>(ref byteA, ref byteB);
+            LogSystem.LogWithColor($"A與B:{byteA} | {byteB}", "#FFF");
+
+            var player1 = new DataPlayer<int>();
+            player1.Data = 999;
+            player1.LogData(999);
+
+            var player2 = new DataPlayer<string>();
+            player2.Data = "motoshin";
+            player2.LogData("motoshin");
         }
-        
+
+        #region 方法區域
         public void SwapNumber(ref int a, ref int b)
         {
             int temp = a;   //將數字A移去旁邊
@@ -47,6 +68,26 @@ namespace motoshin.Class15
             a = b;          //將數字A變成數字B
             b = temp;       //將數字B變成旁邊的數字
         }
+
+        public void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;   //將數字A移去旁邊
+            a = b;          //將數字A變成數字B
+            b = temp;       //將數字B變成旁邊的數字
+        } 
+        #endregion
+
+        public class DataPlayer<T>
+        {
+            public T Data;
+
+            public void LogData(T data)
+            {
+                LogSystem.LogWithColor(data, "#999");
+            }
+        }
+
+
     }
 
 }
